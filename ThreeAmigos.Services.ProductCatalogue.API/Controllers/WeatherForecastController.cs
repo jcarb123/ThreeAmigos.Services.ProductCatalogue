561175bd7d8b300730fbbf0ec0c9315c5cebf2c1
jcarb123 <146824494+jcarb123@users.noreply.github.com>
2023-11-09 15:20:56 +0000

    Update WeatherForecastController.cs

--- ThreeAmigos.Services.ProductCatalogue.API/Controllers/WeatherForecastController.cs
+++ ThreeAmigos.Services.ProductCatalogue.API/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
-        "TYRONETYRONETYRONETYRONETYRONETYRONETYRONETYRONETYRONETYRONETYRONE"
+        "tyrone"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
