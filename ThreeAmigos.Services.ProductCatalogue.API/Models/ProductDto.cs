eff21672a175c0cb00ce1c9a4f92214291013728
jcarb123 <146824494+jcarb123@users.noreply.github.com>
2023-12-11 13:23:46 +0000

    final model changes

--- ThreeAmigos.Services.ProductCatalogue.API/Models/ProductDto.cs
+++ ThreeAmigos.Services.ProductCatalogue.API/Models/ProductDto.cs
@@ -1,37 +1,37 @@
 namespace ThreeAmigos.Services.ProductCatalogue.API.Models;
 
 /// <summary>
-/// Product DTO model
+///     Product DTO model
 /// </summary>
 public class ProductDto
 {
     /// <summary>
-    /// Product name
+    ///     Product name
     /// </summary>
     public string Name { get; set; } = "";
-    
+
     /// <summary>
-    /// Product description
+    ///     Product description
     /// </summary>
     public string Description { get; set; } = "";
-    
+
     /// <summary>
-    /// Product price
+    ///     Product price
     /// </summary>
     public decimal Price { get; set; }
-    
+
     /// <summary>
-    /// Product availability status
+    ///     Product availability status
     /// </summary>
     public bool InStock { get; set; }
-    
+
     /// <summary>
-    /// Product calories
+    ///     Product calories
     /// </summary>
     public int Calories { get; set; }
-    
+
     /// <summary>
-    /// Product image url
+    ///     Product image url
     /// </summary>
     public string ImageUrl { get; set; } = "";
 }
